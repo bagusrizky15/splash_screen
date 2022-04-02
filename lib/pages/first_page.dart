2a4b47385dfd7372540ebd8902c1f1b5b761ab9a
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-04-03 04:00:17 +0700

    add color

---
+++ lib/pages/first_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(FirstSplash());
+}
+
+class FirstSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.white);
+  }
+}
